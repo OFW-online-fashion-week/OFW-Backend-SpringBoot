7813acc93aa38f98aa4f7e41107b055fb8fde17e
junkiYoon <juneki4933@gmail.com>
2021-10-19 21:55:43 +0900

    FIX: delete useless sentences

--- src/main/java/com/ofw/ofw/service/image/AmazonS3Service.java
+++ src/main/java/com/ofw/ofw/service/image/AmazonS3Service.java
@@ -1,10 +1,8 @@
 package com.ofw.ofw.service.image;
 
-import com.amazonaws.services.s3.model.ObjectMetadata;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.io.InputStream;
 
 public interface AmazonS3Service {
 
--- src/main/java/com/ofw/ofw/service/image/AmazonS3ServiceImpl.java
+++ src/main/java/com/ofw/ofw/service/image/AmazonS3ServiceImpl.java
@@ -2,7 +2,6 @@ package com.ofw.ofw.service.image;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
-import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -10,9 +9,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 
 @Slf4j
 @RequiredArgsConstructor
