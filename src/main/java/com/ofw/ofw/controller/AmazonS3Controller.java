8c443c52802e817a4c197b765f367c147e3d857b
junkiYoon <juneki4933@gmail.com>
2021-10-19 22:49:26 +0900

    UPDATE: amazon s3 controller
    add response status created

--- src/main/java/com/ofw/ofw/controller/AmazonS3Controller.java
+++ src/main/java/com/ofw/ofw/controller/AmazonS3Controller.java
@@ -2,10 +2,8 @@ package com.ofw.ofw.controller;
 
 import com.ofw.ofw.service.image.AmazonS3Service;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -18,6 +16,7 @@ public class AmazonS3Controller {
     private final AmazonS3Service amazonS3Service;
 
     @PostMapping("/upload")
+    @ResponseStatus(HttpStatus.CREATED)
     public String upload(@RequestParam("file") MultipartFile multipartFile) throws IOException {
         return amazonS3Service.uploadFile(multipartFile);
     }
