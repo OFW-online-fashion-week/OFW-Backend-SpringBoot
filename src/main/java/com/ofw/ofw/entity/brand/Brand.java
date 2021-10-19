7bf1154ecad7f31ef473534c33913cab844ec3cd
Seung jun <poodle2405@gmail.com>
2021-10-19 19:06:00 +0900

    UPDATE: Brand

--- src/main/java/com/ofw/ofw/entity/brand/Brand.java
+++ src/main/java/com/ofw/ofw/entity/brand/Brand.java
@@ -1,9 +1,18 @@
 package com.ofw.ofw.entity.brand;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Brand {
     @Id
