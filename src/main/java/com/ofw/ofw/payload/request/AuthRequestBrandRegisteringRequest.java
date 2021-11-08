2e2959bc4e55a05675de58e2a3865e93d11fc8c4
Seung jun <poodle2405@gmail.com>
2021-11-08 19:37:50 +0900

    CREATE: AuthRequestBrandRegisteringRequest
    payload

---
+++ src/main/java/com/ofw/ofw/payload/request/AuthRequestBrandRegisteringRequest.java
@@ -0,0 +1,34 @@
+package com.ofw.ofw.payload.request;
+
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.lang.Nullable;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthRequestBrandRegisteringRequest {
+
+    @NotNull
+    private String profileUrl;
+
+    @NotNull
+    private String coverUrl;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private String email;
+
+    @NotNull
+    private String password;
+
+    @NotNull
+    private String url;
+
+    @Nullable
+    private String description;
+}
