07e63c1bb6cd2b0bb2141999a761510d94eb4b28
Seung jun <poodle2405@gmail.com>
2021-11-08 19:32:41 +0900

    CREATE: Collection
    collection entity

---
+++ src/main/java/com/ofw/ofw/entity/collection/Collection.java
@@ -0,0 +1,42 @@
+package com.ofw.ofw.entity.collection;
+
+import com.ofw.ofw.entity.brand.Brand;
+import com.ofw.ofw.entity.runway.Runway;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Collection {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "brand_id")
+    private Brand brand;
+
+    @Column(nullable = false, length = 50)
+    private String title;
+
+    private String description;
+
+    @Column(nullable = false)
+    private Boolean implement;
+
+    @OneToMany(fetch = FetchType.LAZY)
+    private List<Runway> runways;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+}
