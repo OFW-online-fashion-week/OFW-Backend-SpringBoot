5f31b744869226e72ee3c66ab2880ee24aad370f
Seung jun <poodle2405@gmail.com>
2021-11-08 19:08:07 +0900

    CREATE: AuthController

--- src/main/java/com/ofw/ofw/controller/AuthController.java
+++ src/main/java/com/ofw/ofw/controller/AuthController.java
@@ -1,4 +1,31 @@
 package com.ofw.ofw.controller;
 
+import com.ofw.ofw.payload.request.AuthRequestBrandRegisteringRequest;
+import com.ofw.ofw.payload.response.SessionUser;
+import com.ofw.ofw.service.auth.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
+
+@RequestMapping("/auth")
+@RestController
+@RequiredArgsConstructor
 public class AuthController {
+    private final HttpSession httpSession;
+    private  final AuthService authService;
+
+    @GetMapping
+    public String index() {
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        return user.getEmail();
+    }
+
+    @PostMapping("/brand/signup")
+    @ResponseStatus(HttpStatus.CREATED)
+    public String requestBrandRegistering(@RequestBody AuthRequestBrandRegisteringRequest request) {
+        authService.requestBrandRegistering(request);
+        return "success";
+    }
 }
