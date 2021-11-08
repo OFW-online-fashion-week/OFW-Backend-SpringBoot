d9f9229cbf1bc207d17b22ed3dc98d91a058618a
Seung jun <poodle2405@gmail.com>
2021-11-08 19:09:19 +0900

    CREATE: AuthBrandCacheRepository

---
+++ src/main/java/com/ofw/ofw/entity/auth/AuthBrandCacheRepository.java
@@ -0,0 +1,6 @@
+package com.ofw.ofw.entity.auth;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface AuthBrandCacheRepository extends CrudRepository<AuthBrandCache, String> {
+}
