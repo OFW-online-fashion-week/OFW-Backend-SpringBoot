a5c739205529dadddfb67b8d859d7304cb9507d5
Seung jun <poodle2405@gmail.com>
2021-11-16 19:50:59 +0900

    CREATE: jwtTokenProvider

---
+++ src/main/java/com/ofw/ofw/security/jwt/JwtTokenProvider.java
@@ -0,0 +1,99 @@
+package com.ofw.ofw.security.jwt;
+
+import com.ofw.ofw.exception.type.ExpiredAccessTokenException;
+import com.ofw.ofw.security.jwt.auth.AuthDetailsService;
+import io.jsonwebtoken.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.Date;
+
+@Component
+@RequiredArgsConstructor
+public class JwtTokenProvider {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    @Value("${jwt.access.exp}")
+    private Long accessExp;
+
+    @Value("${jwt.refresh.exp}")
+    private Long refreshExp;
+
+    @Value("${jwt.header}")
+    private String header;
+
+    @Value("${jwt.prefix}")
+    private String prefix;
+
+    private final AuthDetailsService authDetailsService;
+
+    public String generateAccessToken(String email) {
+        return Jwts.builder()
+                .signWith(SignatureAlgorithm.HS256, getSecretKey())
+                .setHeaderParam("typ", "JWT")
+                .setSubject(email)
+                .claim("type", "access")
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + accessExp * 1000))
+                .compact();
+    }
+
+    public String generateRefreshToken(String email) {
+        return Jwts.builder()
+                .signWith(SignatureAlgorithm.HS256, getSecretKey())
+                .setHeaderParam("typ", "JWT")
+                .setSubject(email)
+                .claim("type", "refresh")
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + refreshExp * 1000))
+                .compact();
+    }
+
+    public String resolveToken(HttpServletRequest request) {
+        String bearer = request.getHeader(header);
+        if (bearer != null && bearer.startsWith(prefix)) {
+            return bearer.replace(prefix, "");
+        }
+        return null;
+    }
+
+    public boolean validateToken(String token) {
+        return getTokenBody(token)
+                .getExpiration().after(new Date());
+    }
+
+    public Authentication authentication(String token) {
+        UserDetails userDetails = authDetailsService
+                .loadUserByUsername(getTokenSubject(token));
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+    }
+
+    private Claims getTokenBody(String token) {
+        try {
+            return Jwts.parser().setSigningKey(getSecretKey())
+                    .parseClaimsJws(token).getBody();
+        } catch (ExpiredJwtException e) {
+            throw new ExpiredAccessTokenException();
+        } catch (MalformedJwtException e) {
+            throw new InvalidTokenException();
+        }
+    }
+
+    private String getTokenSubject(String token) {
+        return getTokenBody(token).getSubject();
+    }
+
+
+    private String getSecretKey() {
+        return Base64.getEncoder().encodeToString(secretKey.getBytes());
+    }
+}
