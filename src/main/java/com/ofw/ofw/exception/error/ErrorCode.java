c542ea0ada14c7a866a1a9d0223a9701dbf8b92e
Seung jun <poodle2405@gmail.com>
2021-11-16 19:55:27 +0900

    CREATE: ErrorCode

---
+++ src/main/java/com/ofw/ofw/exception/error/ErrorCode.java
@@ -0,0 +1,17 @@
+package com.ofw.ofw.exception.error;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
+public enum ErrorCode {
+    INVALID_INPUT_VALUE(400, "Invalid input value"),
+    EXPIRED_ACCESS_TOKEN(401, "Expired access token"),
+    USER_NOT_FOUND(404, "User not found");
+
+    private final int status;
+    private final String message;
+}
