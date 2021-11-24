bd5d342233f6d79614d3b4678aac4cd590621fe5
Seung jun <poodle2405@gmail.com>
2021-11-24 09:57:20 +0900

    CREATE: CollectionDesignerRepository

---
+++ src/main/java/com/ofw/ofw/entity/collection_designer/CollectionDesignerRepository.java
@@ -0,0 +1,6 @@
+package com.ofw.ofw.entity.collection_designer;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CollectionDesignerRepository extends CrudRepository<CollectionDesigner, Long> {
+}
