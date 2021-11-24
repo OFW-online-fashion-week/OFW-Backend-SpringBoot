a0befb448dbb54ffdea45a51ca75bd7d1abc2386
junkiYoon <juneki4933@gmail.com>
2021-11-24 15:01:23 +0900

    UPDATE: BrandRepository
    change CrudRepository to JpaRepository, and add findByNameStartsWithIgnoreCase method

--- src/main/java/com/ofw/ofw/entity/brand/BrandRepository.java
+++ src/main/java/com/ofw/ofw/entity/brand/BrandRepository.java
@@ -1,4 +1,9 @@
 package com.ofw.ofw.entity.brand;
 
-public interface BrandRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface BrandRepository extends JpaRepository<Brand, Long> {
+    List<Brand> findByNameStartsWithIgnoreCase(String rating);
 }
