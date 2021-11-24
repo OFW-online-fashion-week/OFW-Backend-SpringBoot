ac9d93ca20362e49aa492135e9ac3ec0fadea325
Seung jun <poodle2405@gmail.com>
2021-11-24 11:06:18 +0900

    UPDATE: Like
    update like entity

--- src/main/java/com/ofw/ofw/entity/like/Like.java
+++ src/main/java/com/ofw/ofw/entity/like/Like.java
@@ -1,6 +1,7 @@
 package com.ofw.ofw.entity.like;
 
 import com.ofw.ofw.entity.brand.Brand;
+import com.ofw.ofw.entity.runway.Runway;
 import com.ofw.ofw.entity.user.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -16,12 +17,16 @@ import javax.persistence.*;
 @Entity
 public class Like {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     @ManyToOne(fetch = FetchType.LAZY)
+    @MapsId("brand_id")
     @JoinColumn(name = "brand_id")
     private Brand brand;
 
-    @Id
     @ManyToOne(fetch = FetchType.LAZY)
+    @MapsId("user_id")
     @JoinColumn(name = "user_id")
     private User user;
 
