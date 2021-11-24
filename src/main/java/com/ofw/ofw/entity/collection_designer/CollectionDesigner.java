86d5c5b13878799144011150ce219f307b66e75b
Seung jun <poodle2405@gmail.com>
2021-11-24 09:55:25 +0900

    CREATE: CollectionDesigner

---
+++ src/main/java/com/ofw/ofw/entity/collection_designer/CollectionDesigner.java
@@ -0,0 +1,30 @@
+package com.ofw.ofw.entity.collection_designer;
+
+import com.ofw.ofw.entity.brand.Brand;
+import com.ofw.ofw.entity.collection.Collection;
+import com.ofw.ofw.entity.designer.Designer;
+import com.ofw.ofw.entity.user.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class CollectionDesigner {
+    @Id
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "collection_id")
+    private Collection collection;
+
+    @Id
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "designer_id")
+    private Designer designer;
+
+}
