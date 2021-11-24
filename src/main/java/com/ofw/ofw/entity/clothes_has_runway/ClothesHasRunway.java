0a5937e076d1bcb7dd3b979e8dec2b17377bc5a1
Seung jun <poodle2405@gmail.com>
2021-11-24 11:04:34 +0900

    CREATE: ClothesHasRunway
    create clothes has runway entity

---
+++ src/main/java/com/ofw/ofw/entity/clothes_has_runway/ClothesHasRunway.java
@@ -0,0 +1,32 @@
+package com.ofw.ofw.entity.clothes_has_runway;
+
+import com.ofw.ofw.entity.clothes.Clothes;
+import com.ofw.ofw.entity.runway.Runway;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class ClothesHasRunway {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @MapsId("clothes_id")
+    @JoinColumn(name = "clothes_id")
+    private Clothes clothes;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @MapsId("runway_id")
+    @JoinColumn(name = "runway_id")
+    private Runway runway;
+}
