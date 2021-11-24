a319a64f6d3cfe57a83f1ec685fe9a2d67567dda
Seung jun <poodle2405@gmail.com>
2021-11-24 12:42:08 +0900

    UPDATE: CollectionDesigner

--- src/main/java/com/ofw/ofw/entity/collection_designer/CollectionDesigner.java
+++ src/main/java/com/ofw/ofw/entity/collection_designer/CollectionDesigner.java
@@ -1,8 +1,10 @@
 package com.ofw.ofw.entity.collection_designer;
 
 import com.ofw.ofw.entity.brand.Brand;
+import com.ofw.ofw.entity.clothes.Clothes;
 import com.ofw.ofw.entity.collection.Collection;
 import com.ofw.ofw.entity.designer.Designer;
+import com.ofw.ofw.entity.runway.Runway;
 import com.ofw.ofw.entity.user.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -18,13 +20,19 @@ import javax.persistence.*;
 @Entity
 public class CollectionDesigner {
     @Id
+    private Long collection_id;
+
+    @Id
+    private Long designer_id;
+
     @ManyToOne(fetch = FetchType.LAZY)
+    @MapsId("collection_id")
     @JoinColumn(name = "collection_id")
     private Collection collection;
 
-    @Id
+
     @ManyToOne(fetch = FetchType.LAZY)
+    @MapsId("designer_id")
     @JoinColumn(name = "designer_id")
     private Designer designer;
-
 }
