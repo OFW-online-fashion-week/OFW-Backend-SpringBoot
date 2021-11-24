c8e9f87f5274c56965722dfb91a58e80cbe0f5cf
Seung jun <poodle2405@gmail.com>
2021-11-24 11:06:58 +0900

    CREATE: Profile
    create profile entity

---
+++ src/main/java/com/ofw/ofw/entity/profile/Profile.java
@@ -0,0 +1,34 @@
+package com.ofw.ofw.entity.profile;
+
+import com.ofw.ofw.entity.model.Model;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Profile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @MapsId("model_id")
+    @JoinColumn(name = "model_id")
+    private Model model;
+
+    @Column
+    private Long height;
+
+    @Column
+    private Long weight;
+
+    @Column
+    private Long nation;
+}
