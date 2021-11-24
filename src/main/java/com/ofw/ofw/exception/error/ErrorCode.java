551cf26c40de85baccaa1f9df1b6a4a5421f8419
junkiYoon <juneki4933@gmail.com>
2021-11-24 22:26:28 +0900

    ADD: ErrorCode
    add BRAND_NOT_FOUND

--- src/main/java/com/ofw/ofw/exception/error/ErrorCode.java
+++ src/main/java/com/ofw/ofw/exception/error/ErrorCode.java
@@ -11,7 +11,8 @@ public enum ErrorCode {
     INVALID_INPUT_VALUE(400, "Invalid input value"),
     EXPIRED_ACCESS_TOKEN(401, "Expired access token"),
     USER_NOT_FOUND(404, "User not found"),
-    BRAND_CACHE_NOT_FOUND(404, "Brand cache not found");
+    BRAND_CACHE_NOT_FOUND(404, "Brand cache not found"),
+    BRAND_NOT_FOUND(404, "Brand not found");
 
     private final int status;
     private final String message;
