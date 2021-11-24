e1a57aaca3680db914c5e9fdbfd3de02f09189db
Seung jun <poodle2405@gmail.com>
2021-11-24 11:03:48 +0900

    CREATE: Clothes
    create Clothes entity

---
+++ src/main/java/com/ofw/ofw/entity/clothes/Clothes.java
@@ -0,0 +1,39 @@
+package com.ofw.ofw.entity.clothes;
+
+import com.ofw.ofw.entity.brand.Brand;
+import com.ofw.ofw.entity.collection.Collection;
+import com.ofw.ofw.entity.like.Like;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Clothes {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 250)
+    private String image_path;
+
+    @Column( length = 25)
+    private String style_code;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @Column( length = 100)
+    private String url;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "brand_id")
+    private Brand brand;
+}
