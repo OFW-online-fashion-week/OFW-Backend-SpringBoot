0ebf53e9969b98938a26c703326592428320d1ac
Seung jun <poodle2405@gmail.com>
2021-11-24 11:02:59 +0900

    CREATE: ClothesHasRunwayRepository

---
+++ src/main/java/com/ofw/ofw/entity/clothes_has_runway/ClothesHasRunwayRepository.java
@@ -0,0 +1,6 @@
+package com.ofw.ofw.entity.clothes_has_runway;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ClothesHasRunwayRepository extends CrudRepository<ClothesHasRunway, Long> {
+}
