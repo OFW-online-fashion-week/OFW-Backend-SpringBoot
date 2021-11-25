84973cb9dd3a5f4092cbe63039dd1145b30cdd6f
Seung jun <poodle2405@gmail.com>
2021-11-25 22:06:46 +0900

    CREATE: SecurityConfig

---
+++ src/main/java/com/ofw/ofw/security/SecurityConfig.java
@@ -0,0 +1,44 @@
+package com.ofw.ofw.security;
+
+import com.ofw.ofw.exception.ExceptionHandlerFilter;
+import com.ofw.ofw.security.jwt.JwtTokenProvider;
+import com.ofw.ofw.security.jwt.FilterConfigure;
+import com.ofw.ofw.security.logging.RequestLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+    private final ExceptionHandlerFilter exceptionHandlerFilter;
+    private final RequestLogger requestLogger;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .formLogin().disable()
+                .csrf().disable()
+                .cors().and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/swagger-ui.html").permitAll()
+                .antMatchers(HttpMethod.GET, "/users/auth/google").permitAll()
+                .antMatchers(HttpMethod.POST, "/users/auth/google").permitAll()
+                .antMatchers(HttpMethod.GET, "/club/{id}").permitAll()
+                .antMatchers(HttpMethod.GET, "/profile/{id}").permitAll()
+                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                .anyRequest().authenticated()
+                .and().apply(new FilterConfigure(jwtTokenProvider, exceptionHandlerFilter, requestLogger));
+    }
+}
