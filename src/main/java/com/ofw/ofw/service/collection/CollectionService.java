c0f3c1a37a6d5598e4bafc4ba05f1593f997a88d
Seung jun <poodle2405@gmail.com>
2021-11-25 22:56:04 +0900

    UPDATE: CollectionService

--- src/main/java/com/ofw/ofw/service/collection/CollectionService.java
+++ src/main/java/com/ofw/ofw/service/collection/CollectionService.java
@@ -1,8 +1,10 @@
 package com.ofw.ofw.service.collection;
 
 import com.ofw.ofw.payload.collection.response.CollectionListResponse;
+import com.ofw.ofw.payload.collection.response.CollectionResponse;
 import org.springframework.data.domain.Pageable;
 
 public interface CollectionService {
     CollectionListResponse getCollectionList(Pageable pageable);
+    CollectionResponse getCollection(Long collection_id);
 }
