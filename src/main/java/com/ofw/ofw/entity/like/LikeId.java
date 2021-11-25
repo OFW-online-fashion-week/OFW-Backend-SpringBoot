54346b57e7bee5ab7a0d29bba53f96aa66bf6636
Seung jun <poodle2405@gmail.com>
2021-11-25 22:54:40 +0900

    CREATE: LikeId

---
+++ src/main/java/com/ofw/ofw/entity/like/LikeId.java
@@ -0,0 +1,23 @@
+package com.ofw.ofw.entity.like;
+
+import com.ofw.ofw.entity.brand.Brand;
+import com.ofw.ofw.entity.user.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Getter
+@NoArgsConstructor
+@Embeddable
+public class LikeId implements Serializable {
+    @Column(name = "brand_id")
+    private Long brandId;
+
+    @Column(name = "user_id")
+    private Long userId;
+
+}
