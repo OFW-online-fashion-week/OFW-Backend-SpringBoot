e328c20f7895c00d1c4e5e380a3805f3ca388e7b
Seung jun <poodle2405@gmail.com>
2021-11-25 22:55:44 +0900

    UPDATE: AuthService

--- src/main/java/com/ofw/ofw/service/auth/AuthService.java
+++ src/main/java/com/ofw/ofw/service/auth/AuthService.java
@@ -1,7 +1,13 @@
 package com.ofw.ofw.service.auth;
 
 import com.ofw.ofw.payload.auth.request.AuthRequestBrandRegisteringRequest;
+import com.ofw.ofw.payload.auth.request.GetGoogleTokenByCodeRequest;
+import com.ofw.ofw.payload.auth.response.LinkResponse;
+import com.ofw.ofw.payload.auth.response.TokenResponse;
 
 public interface AuthService {
     void requestBrandRegistering(AuthRequestBrandRegisteringRequest request);
+
+    LinkResponse getGoogleLink();
+    TokenResponse getGoogleTokenByCode(GetGoogleTokenByCodeRequest request);
 }
