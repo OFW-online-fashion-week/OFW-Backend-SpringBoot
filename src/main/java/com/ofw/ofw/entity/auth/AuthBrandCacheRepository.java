e8bc530e513847df27382f872f3ed0f0382ef9b6
Seung jun <poodle2405@gmail.com>
2021-11-25 22:52:59 +0900

    UPDATE: AuthBrandCacheRepository

--- src/main/java/com/ofw/ofw/entity/auth/AuthBrandCacheRepository.java
+++ src/main/java/com/ofw/ofw/entity/auth/AuthBrandCacheRepository.java
@@ -1,6 +1,7 @@
 package com.ofw.ofw.entity.auth;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface AuthBrandCacheRepository extends JpaRepository<AuthBrandCache, String> {
+public interface AuthBrandCacheRepository extends CrudRepository<AuthBrandCache, String> {
 }
