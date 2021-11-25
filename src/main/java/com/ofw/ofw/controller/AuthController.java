5bab2e4bd838c06edb755f838fbfce4da97c8c13
Seung jun <poodle2405@gmail.com>
2021-11-25 22:52:35 +0900

    UPDATE: AuthController

--- src/main/java/com/ofw/ofw/controller/AuthController.java
+++ src/main/java/com/ofw/ofw/controller/AuthController.java
@@ -1,9 +1,13 @@
 package com.ofw.ofw.controller;
 
 import com.ofw.ofw.payload.auth.request.AuthRequestBrandRegisteringRequest;
+import com.ofw.ofw.payload.auth.request.GetGoogleTokenByCodeRequest;
+import com.ofw.ofw.payload.auth.response.LinkResponse;
+import com.ofw.ofw.payload.auth.response.TokenResponse;
 import com.ofw.ofw.service.auth.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
@@ -12,9 +16,17 @@ import javax.servlet.http.HttpSession;
 @RestController
 @RequiredArgsConstructor
 public class AuthController {
-    private final HttpSession httpSession;
     private  final AuthService authService;
 
+    @GetMapping("auth/google")
+    public ResponseEntity<LinkResponse> getGoogleLink() {
+        return new ResponseEntity<>(authService.getGoogleLink(), HttpStatus.OK);
+    }
+
+    @PostMapping("auth/google")
+    public ResponseEntity<TokenResponse> getGoogleTokenByCode(@RequestBody GetGoogleTokenByCodeRequest request) {
+        return new ResponseEntity<>(authService.getGoogleTokenByCode(request), HttpStatus.OK);
+    }
 
     @PostMapping("/brand/signup")
     @ResponseStatus(HttpStatus.CREATED)
