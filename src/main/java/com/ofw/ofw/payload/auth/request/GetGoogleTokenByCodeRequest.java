b81e97959cbd71e0eb75d432abad9cde92a0bafd
Seung jun <poodle2405@gmail.com>
2021-11-25 22:04:56 +0900

    CREATE: GetGoogleTokenByCodeRequest

---
+++ src/main/java/com/ofw/ofw/payload/auth/request/GetGoogleTokenByCodeRequest.java
@@ -0,0 +1,13 @@
+package com.ofw.ofw.payload.auth.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class GetGoogleTokenByCodeRequest {
+
+    private String code;
+    private String name;
+    private String aud;
+}
