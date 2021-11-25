903d51947420c711d4f0b23d259a0bd6a299ff75
Seung jun <poodle2405@gmail.com>
2021-11-25 22:53:08 +0900

    UPDATE: CollectionRepository

--- src/main/java/com/ofw/ofw/entity/collection/CollectionRepository.java
+++ src/main/java/com/ofw/ofw/entity/collection/CollectionRepository.java
@@ -2,10 +2,10 @@ package com.ofw.ofw.entity.collection;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CollectionRepository extends CrudRepository<Collection, Long> {
+public interface CollectionRepository extends JpaRepository<Collection, Long>, QuerydslRepository {
     Page<Collection> findAllByOrderByCreatedAt(Pageable pageable);
 }
