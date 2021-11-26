68e98311fbbab934ea237f6bed843de871a72527
junkiYoon <juneki4933@gmail.com>
2021-11-26 13:30:35 +0900

    FIX: LikeRepository
    change id type to LikeId

--- src/main/java/com/ofw/ofw/entity/like/LikeRepository.java
+++ src/main/java/com/ofw/ofw/entity/like/LikeRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LikeRepository extends CrudRepository<Like, Long> {
+public interface LikeRepository extends CrudRepository<Like, LikeId> {
 }
 
