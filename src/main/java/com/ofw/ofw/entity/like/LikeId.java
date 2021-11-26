c7f4cf8b58e4b5561fa66f8578e8298bd303ef4d
Seung jun <poodle2405@gmail.com>
2021-11-26 14:33:09 +0900

    FIX: LikedId
    import AllArgsConstructor

--- src/main/java/com/ofw/ofw/entity/like/LikeId.java
+++ src/main/java/com/ofw/ofw/entity/like/LikeId.java
@@ -1,7 +1,5 @@
 package com.ofw.ofw.entity.like;
 
-import com.ofw.ofw.entity.brand.Brand;
-import com.ofw.ofw.entity.user.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,6 +11,7 @@ import java.io.Serializable;
 @Builder
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 @Embeddable
 public class LikeId implements Serializable {
     @Column(name = "brand_id")
