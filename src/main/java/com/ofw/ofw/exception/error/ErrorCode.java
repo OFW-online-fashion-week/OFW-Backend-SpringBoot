848c961df05434c16eaa65af85c21edf42460365
junkiYoon <juneki4933@gmail.com>
2021-11-26 13:36:04 +0900

    UPDATE: ErrorCode
    add RUNWAY_NOT_FOUND

--- src/main/java/com/ofw/ofw/exception/error/ErrorCode.java
+++ src/main/java/com/ofw/ofw/exception/error/ErrorCode.java
@@ -13,7 +13,8 @@ public enum ErrorCode {
     USER_NOT_FOUND(404, "User not found"),
     BRAND_CACHE_NOT_FOUND(404, "Brand cache not found"),
     BRAND_NOT_FOUND(404, "Brand not found"),
-    CREDENTIALS_NOT_FOUND(401, "Credentials not found");
+    CREDENTIALS_NOT_FOUND(401, "Credentials not found"),
+    RUNWAY_NOT_FOUND(404, "Runway not found");
 
     private final int status;
     private final String message;
