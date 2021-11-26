2669b9846bb8e6a6cb67dc9fa9421427342a3a58
junkiYoon <juneki4933@gmail.com>
2021-11-26 13:31:08 +0900

    UPDATE: AuthBrandCacheRepository
    change CurdRepository to JpaRepository

--- src/main/java/com/ofw/ofw/entity/auth/AuthBrandCacheRepository.java
+++ src/main/java/com/ofw/ofw/entity/auth/AuthBrandCacheRepository.java
@@ -1,7 +1,6 @@
 package com.ofw.ofw.entity.auth;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 
-public interface AuthBrandCacheRepository extends CrudRepository<AuthBrandCache, String> {
+public interface AuthBrandCacheRepository extends JpaRepository<AuthBrandCache, String> {
 }
