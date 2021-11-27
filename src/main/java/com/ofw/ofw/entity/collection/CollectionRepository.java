8da58a6daaf399afac36377f62d27635fe2e2f69
Seung jun <poodle2405@gmail.com>
2021-11-27 14:47:51 +0900

    UPDATE: CollectionRepository
    update findAllbyByBrand

--- src/main/java/com/ofw/ofw/entity/collection/CollectionRepository.java
+++ src/main/java/com/ofw/ofw/entity/collection/CollectionRepository.java
@@ -5,7 +5,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CollectionRepository extends JpaRepository<Collection, Long>, QuerydslRepository {
     Page<Collection> findAllByOrderByCreatedAt(Pageable pageable);
+    List<Collection> findAllByBrand(Long brandId);
 }
