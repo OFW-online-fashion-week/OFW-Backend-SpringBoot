bfedd473ad71462bc931acb3b4dc22f81f9b061f
Seung jun <poodle2405@gmail.com>
2021-11-27 20:50:55 +0900

    UPDATE: QuerydslRepositoryImpl
    update getDesigner

--- src/main/java/com/ofw/ofw/entity/collection/QuerydslRepositoryImpl.java
+++ src/main/java/com/ofw/ofw/entity/collection/QuerydslRepositoryImpl.java
@@ -2,6 +2,8 @@ package com.ofw.ofw.entity.collection;
 
 import com.ofw.ofw.payload.collection.response.CollectionResponse;
 import com.ofw.ofw.payload.collection.response.QCollectionResponse;
+import com.ofw.ofw.payload.designer.response.CollectionDesignerResponse;
+import com.ofw.ofw.payload.designer.response.QCollectionDesignerResponse;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -37,5 +39,29 @@ public class QuerydslRepositoryImpl implements QuerydslRepository {
                 .where(collectionDesigner.collection.eq(collections))
                 .fetchOne();
     }
+
+    @Override
+    public CollectionDesignerResponse getCollectionDesigner(Long collectionId){
+        Collection collections = queryFactory
+                .select(QCollection.collection)
+                .from(QCollection.collection)
+                .where(QCollection.collection.id.eq(collectionId))
+                .fetchOne();
+
+        return queryFactory
+                .select(new QCollectionDesignerResponse(
+                        designer.email.as("designer_email"),
+                        designer.name.as("designer_name"),
+                        designer.description.as("designer_description"),
+                        designer.profilePath.as("designer_profilePath")
+                        )
+                )
+                .from(collection)
+                .join(collectionDesigner.collection, collection)
+                .join(collectionDesigner.designer, designer)
+                .where(collectionDesigner.collection.eq(collections))
+                .fetchOne();
+    }
+
 
 }
