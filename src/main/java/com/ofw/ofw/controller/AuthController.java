9c7b12a5e267f127a6764a8a7ec07a22f62510ac
Seung jun <poodle2405@gmail.com>
2021-11-27 23:49:55 +0900

    UPDATE: AuthController

--- src/main/java/com/ofw/ofw/controller/AuthController.java
+++ src/main/java/com/ofw/ofw/controller/AuthController.java
@@ -2,6 +2,7 @@ package com.ofw.ofw.controller;
 
 import com.ofw.ofw.payload.auth.request.AuthRequestBrandRegisteringRequest;
 import com.ofw.ofw.payload.auth.request.GetGoogleTokenByCodeRequest;
+import com.ofw.ofw.payload.auth.request.SignInBrandRequest;
 import com.ofw.ofw.payload.auth.response.LinkResponse;
 import com.ofw.ofw.payload.auth.response.TokenResponse;
 import com.ofw.ofw.service.auth.AuthService;
@@ -9,8 +10,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import javax.servlet.http.HttpSession;
 
 @RequestMapping("/auth")
 @RestController
@@ -33,5 +32,11 @@ public class AuthController {
     public String requestBrandRegistering(@RequestBody AuthRequestBrandRegisteringRequest request) {
         authService.requestBrandRegistering(request);
         return "success";
+    }
+
+    @PostMapping("/brand/signin")
+    @ResponseStatus(HttpStatus.CREATED)
+    public TokenResponse brandSignIn(@RequestBody SignInBrandRequest request){
+        return authService.brandSignIn(request);
     }
 }
