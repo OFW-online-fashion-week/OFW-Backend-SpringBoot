3762bcef90d3ee2b321854223c0c7ebd973d2d98
Seung jun <poodle2405@gmail.com>
2021-11-27 23:50:04 +0900

    UPDATE: BrandRepository

--- src/main/java/com/ofw/ofw/entity/brand/BrandRepository.java
+++ src/main/java/com/ofw/ofw/entity/brand/BrandRepository.java
@@ -3,7 +3,9 @@ package com.ofw.ofw.entity.brand;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface BrandRepository extends JpaRepository<Brand, Long> {
     List<Brand> findByNameStartsWithIgnoreCase(String rating);
+    Optional<Brand> findByEmail(String email);
 }
