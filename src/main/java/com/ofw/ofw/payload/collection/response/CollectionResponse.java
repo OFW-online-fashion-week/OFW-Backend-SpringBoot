ff231fe35302aef9f50803738809ad6e2c151383
Seung jun <poodle2405@gmail.com>
2021-11-27 20:52:11 +0900

    UPDATE: CollectionResponse

--- src/main/java/com/ofw/ofw/payload/collection/response/CollectionResponse.java
+++ src/main/java/com/ofw/ofw/payload/collection/response/CollectionResponse.java
@@ -1,7 +1,6 @@
 package com.ofw.ofw.payload.collection.response;
 
 import com.querydsl.core.annotations.QueryProjection;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
