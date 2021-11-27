dc14e9f0dd5bb44ebe5156376113f36348b291c7
Seung jun <poodle2405@gmail.com>
2021-11-27 20:53:54 +0900

    UPDATE: DesignerServiceImpl
    update getDesigner

--- src/main/java/com/ofw/ofw/service/designer/DesignerServiceImpl.java
+++ src/main/java/com/ofw/ofw/service/designer/DesignerServiceImpl.java
@@ -1,8 +1,12 @@
 package com.ofw.ofw.service.designer;
 
+import com.ofw.ofw.entity.collection.CollectionRepository;
+import com.ofw.ofw.entity.collection_designer.CollectionDesignerRepository;
 import com.ofw.ofw.entity.designer.Designer;
 import com.ofw.ofw.entity.designer.DesignerRepository;
+import com.ofw.ofw.exception.type.DesignerNotFoundException;
 import com.ofw.ofw.payload.designer.request.SearchDesignerRequest;
+import com.ofw.ofw.payload.designer.response.CollectionDesignerResponse;
 import com.ofw.ofw.payload.designer.response.SerachDesignerResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,6 +15,8 @@ import org.springframework.stereotype.Service;
 @Service
 public class DesignerServiceImpl implements DesignerService{
     private final DesignerRepository designerRepository;
+    private final CollectionRepository collectionRepository;
+
 
     @Override
     public SerachDesignerResponse searchDesigner(SearchDesignerRequest request){
@@ -18,4 +24,12 @@ public class DesignerServiceImpl implements DesignerService{
 
         return new SerachDesignerResponse(designer);
     }
+
+    @Override
+    public CollectionDesignerResponse getDesigner(Long collectionId){
+        CollectionDesignerResponse designer = collectionRepository.getCollectionDesigner(collectionId);
+
+        return new CollectionDesignerResponse(designer);
+    }
+
 }
