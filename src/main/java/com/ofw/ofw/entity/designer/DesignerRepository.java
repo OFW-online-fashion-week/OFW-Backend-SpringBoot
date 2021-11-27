16432e8a697e1edf3379803ceb0932ec1ad13b0d
Seung jun <poodle2405@gmail.com>
2021-11-27 18:02:00 +0900

    UPDATE: DesignerRepository
    update searchDesigner

--- src/main/java/com/ofw/ofw/entity/designer/DesignerRepository.java
+++ src/main/java/com/ofw/ofw/entity/designer/DesignerRepository.java
@@ -1,6 +1,8 @@
 package com.ofw.ofw.entity.designer;
 
+import com.ofw.ofw.payload.designer.response.SerachDesignerResponse;
 import org.springframework.data.repository.CrudRepository;
 
 public interface DesignerRepository extends CrudRepository<Designer, Long> {
+    SerachDesignerResponse findByNameIsLike(String name);
 }
