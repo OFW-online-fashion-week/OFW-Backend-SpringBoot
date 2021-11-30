ea4131fd93c5321da2064e97c08dabf8bd91b7b1
Seung jun <poodle2405@gmail.com>
2021-12-01 08:27:45 +0900

    UPDATE: DesignerRepository

--- src/main/java/com/ofw/ofw/entity/designer/DesignerRepository.java
+++ src/main/java/com/ofw/ofw/entity/designer/DesignerRepository.java
@@ -1,8 +1,7 @@
 package com.ofw.ofw.entity.designer;
 
-import com.ofw.ofw.payload.designer.response.SearchDesignerResponse;
 import org.springframework.data.repository.CrudRepository;
 
 public interface DesignerRepository extends CrudRepository<Designer, Long> {
-    SearchDesignerResponse findByNameStartsWithIgnoreCase(String name);
+    Designer findByNameContaining(String name);
 }
