9c47a3f8d3febf5b232535818788008ff47043f9
Seung jun <poodle2405@gmail.com>
2021-12-01 00:39:23 +0900

    UPDATE: BrandDetailsService

--- src/main/java/com/ofw/ofw/security/jwt/auth/BrandDetailsService.java
+++ src/main/java/com/ofw/ofw/security/jwt/auth/BrandDetailsService.java
@@ -2,6 +2,7 @@ package com.ofw.ofw.security.jwt.auth;
 
 import com.ofw.ofw.entity.brand.BrandRepository;
 import com.ofw.ofw.entity.user.UserRepository;
+import com.ofw.ofw.exception.type.BrandNotFoundException;
 import com.ofw.ofw.exception.type.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -19,6 +20,6 @@ public class BrandDetailsService implements UserDetailsService {
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         return brandRepository.findByEmail(email)
                 .map(BrandDetails::new)
-                .orElseThrow(UserNotFoundException::new);
+                .orElseThrow(BrandNotFoundException::new);
     }
 }
