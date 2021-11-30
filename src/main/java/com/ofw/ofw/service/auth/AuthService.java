31b186d2e2b5e8ce47cb66f72f873c0e04417a93
Seung jun <poodle2405@gmail.com>
2021-11-30 16:55:54 +0900

    UPDATE: AuthService

--- src/main/java/com/ofw/ofw/service/auth/AuthService.java
+++ src/main/java/com/ofw/ofw/service/auth/AuthService.java
@@ -1,15 +1,12 @@
 package com.ofw.ofw.service.auth;
 
-import com.ofw.ofw.payload.auth.request.AuthRequestBrandRegisteringRequest;
-import com.ofw.ofw.payload.auth.request.GetGoogleTokenByCodeRequest;
-import com.ofw.ofw.payload.auth.request.SignInBrandRequest;
+import com.ofw.ofw.payload.auth.request.*;
 import com.ofw.ofw.payload.auth.response.LinkResponse;
 import com.ofw.ofw.payload.auth.response.TokenResponse;
-import org.springframework.web.bind.annotation.RequestBody;
 
 public interface AuthService {
     void requestBrandRegistering(AuthRequestBrandRegisteringRequest request);
     LinkResponse getGoogleLink();
-    TokenResponse getGoogleTokenByCode(GetGoogleTokenByCodeRequest request);
+    TokenResponse googleOauthLogIn(googleOauthLogInRequest request);
     TokenResponse brandSignIn(SignInBrandRequest request);
 }
