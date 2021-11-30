8a11522a6744dbe917d72308c5fdfe15a6b8ccf9
Seung jun <poodle2405@gmail.com>
2021-12-01 08:44:08 +0900

    UPDATE: DesignerService

--- src/main/java/com/ofw/ofw/service/designer/DesignerService.java
+++ src/main/java/com/ofw/ofw/service/designer/DesignerService.java
@@ -3,9 +3,10 @@ package com.ofw.ofw.service.designer;
 import com.ofw.ofw.payload.designer.request.CreateDesignerRequest;
 import com.ofw.ofw.payload.designer.response.CollectionDesignerResponse;
 import com.ofw.ofw.payload.designer.response.SearchDesignerResponse;
+import com.ofw.ofw.payload.designer.response.SearchDesignerResponseList;
 
 public interface DesignerService {
-    SearchDesignerResponse searchDesigner(String name);
+    SearchDesignerResponseList searchDesigner(String name);
     CollectionDesignerResponse getDesigner(Long collectionId);
     void createDesigner(CreateDesignerRequest request);
 }
